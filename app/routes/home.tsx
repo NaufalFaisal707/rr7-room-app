163f4ddda0e0b85740c54869fbd4b5de99bbc4f6
Naufal Faisal <naufalfaisal707@gmail.com>
2025-03-13 08:21:57 +0700

    Fix development scripts and update test message
    Updates dev script to use nodemon and corrects start command to run
    without watch mode

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,7 +9,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: "anjay" };
+  return { message: "wello horld" };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
