077a0f54fab993faaa1132b5be61cf09f58db5f0
Naufal Faisal <naufalfaisal707@gmail.com>
2025-03-13 10:46:40 +0700

    Remove JWT and cookie authentication

--- server/app.ts
+++ server/app.ts
@@ -4,32 +4,10 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 const { hash } = bcrypt;
-import {
-  accessCookie,
-  refreshCookie,
-  clearAccessCookie,
-  clearRefreshCookie,
-} from "./lib/cookie";
-import {
-  generateAccessToken,
-  generateRefreshToken,
-  verifyAccessToken,
-  verifyRefreshToken,
-} from "./lib/jwt";
-import type { Cookie } from "react-router";
-import type { JwtPayload } from "jsonwebtoken";
 
 declare module "react-router" {
   interface AppLoadContext {
     prisma: PrismaClient;
-    accessCookie: Cookie;
-    refreshCookie: Cookie;
-    clearAccessCookie: Cookie;
-    clearRefreshCookie: Cookie;
-    generateAccessToken: (value: string) => string;
-    generateRefreshToken: (value: string) => string;
-    verifyAccessToken: (token: string) => string | JwtPayload | null;
-    verifyRefreshToken: (token: string) => string | JwtPayload | null;
   }
 }
 
@@ -58,23 +36,9 @@ app.use(
   createRequestHandler({
     // @ts-expect-error - virtual module provided by React Router at build time
     build: () => import("virtual:react-router/server-build"),
-    // @ts-ignore
     getLoadContext() {
       return {
-        // prisma
-        prisma,
-
-        // cookie
-        accessCookie,
-        refreshCookie,
-        clearAccessCookie,
-        clearRefreshCookie,
-
-        // jwt token
-        generateAccessToken,
-        generateRefreshToken,
-        verifyAccessToken,
-        verifyRefreshToken,
+        prisma: prisma as PrismaClient,
       };
     },
   })
